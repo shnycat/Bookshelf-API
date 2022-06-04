7087e0ebcf84970953fcde4edc7d9220e9915681
shnycat <shnycat@gmail.com>
2022-06-04 17:27:23 +0700

    Kriteria 4: 'dapat mengedit buku' DONE

--- src/handler.js
+++ src/handler.js
@@ -103,8 +103,75 @@ const getBookByIdHandler = (request, h) => {
   return response;
 };
 
+const editBookHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const idInvalid = books.filter((b) => b.id === bookId).length === 0;
+  if (idInvalid) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+  }
+
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  if (!name || readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: `Gagal memperbarui buku. ${
+        !name
+          ? 'Mohon isi nama buku'
+          : 'readPage tidak boleh lebih besar dari pageCount'
+      }`,
+    });
+
+    response.code(400);
+    return response;
+  }
+
+  const updatedAt = new Date().toISOString();
+
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index !== -1) {
+    books[index] = {
+      ...books[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui',
+    });
+
+    response.code(200);
+    return response;
+  }
+};
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
+  editBookHandler,
 };
