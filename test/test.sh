c40b98eacc7a8419bd6bad6c12d2d893814401ca
shnycat <shnycat@gmail.com>
2022-06-05 02:49:59 +0700

    modularize handler

--- test/test.sh
+++ test/test.sh
@@ -5,16 +5,24 @@ url_='http://localhost:5000'
 function main() {
   case "$1" in
     --add)
-      curl -X POST                          \
+      curl -X POST "$url_/books"            \
         -H "Content-Type: application/json" \
-        -d "@data.json"                     \
-        "$url_/books"
+        -d "@data.json"
+        
       ;;
     --all)
       curl -X GET "$url_/books"
       ;;
     --id)
       curl -X GET "$url_/books/$2"
+      ;;
+    --edit)
+      curl -X PUT "$url_/books/$2"          \
+        -H "Content-Type: application/json" \
+        -d "@book.json"
+      ;;
+    --del)
+      curl -X DELETE "$url_/books/$2"
       ;;
   esac 
 } 
