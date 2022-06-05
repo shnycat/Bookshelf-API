e5e48fda1aa74631f33467a26139614009228537
shnycat <shnycat@gmail.com>
2022-06-06 02:27:03 +0700

    fixed unmatched kriteria, id, name, publisher only

--- src/handler/getAllBooks.js
+++ src/handler/getAllBooks.js
@@ -2,7 +2,14 @@ const books = require('../books');
 
 const getBooks = () => ({
   status: 'success',
-  data: books.length > 0 ? { books } : { books: [] },
+  data:
+    books.length > 0
+      ? {
+          id: books.id,
+          name: books.name,
+          publisher: books.publisher,
+        }
+      : { books: [] },
 });
 
 module.exports = getBooks;
