7dc6126244cf64f6f09aa3234de24b2b53a27798
shnycat <shnycat@gmail.com>
2022-06-11 13:51:03 +0700

    added postman collection api test

--- test/test.sh
+++ test/test.sh
@@ -1,30 +1,32 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 url_='http://localhost:5000'
+tmp_=$PREFIX/tmp/response.json
 
 function main() {
-  case "$1" in
-    --add)
-      curl -X POST "$url_/books"            \
-        -H "Content-Type: application/json" \
-        -d "@data.json"
-        
-      ;;
-    --all)
-      curl -X GET "$url_/books"
-      ;;
-    --id)
-      curl -X GET "$url_/books/$2"
-      ;;
-    --edit)
-      curl -X PUT "$url_/books/$2"          \
-        -H "Content-Type: application/json" \
-        -d "@book.json"
-      ;;
-    --del)
-      curl -X DELETE "$url_/books/$2"
-      ;;
-  esac 
+  {
+    case "$1" in
+      --add)
+        curl -s -X POST "$url_/books"            \
+          -H "Content-Type: application/json" \
+          -d "@data.json"
+        ;;
+      --all)
+        curl -s -X GET "$url_/books"
+        ;;
+      --id)
+        curl -s -X GET "$url_/books/$2"
+        ;;
+      --edit)
+        curl -s -X PUT "$url_/books/$2"          \
+          -H "Content-Type: application/json" \
+          -d "@book.json"
+        ;;
+      --del)
+        curl -s -X DELETE "$url_/books/$2"
+        ;;
+    esac
+  } | jq
 } 
 
 main $1 $2
